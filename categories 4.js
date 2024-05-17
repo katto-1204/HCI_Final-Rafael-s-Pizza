5d9a0eed50be93f488875ca1715357618b62a03f
katto-1204 <149875732+katto-1204@users.noreply.github.com>
2024-05-17 20:49:37 +0800

    Add files via upload

---
+++ categories 4.js
@@ -0,0 +1,14 @@
+var arrows = document.querySelectorAll(".arrow-main");
+
+arrows.forEach(function(arrow) {
+  arrow.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    if (!arrow.classList.contains("animate")) {
+      arrow.classList.add("animate");
+      setTimeout(function() {
+        arrow.classList.remove("animate");
+      }, 1600);
+    }
+  });
+});
